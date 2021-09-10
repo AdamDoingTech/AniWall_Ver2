9fde33e7c43ef71345d994917c5581f34448c9f2
Adam <olmpy@hotmail.com>
2021-09-09 21:59:58 -0600

    Removed the new method, and put the old method back into place, and renamed the gestures funcs, and renamed the gesture vars.

--- AniWallPaperAppV2/ViewController.swift
+++ AniWallPaperAppV2/ViewController.swift
@@ -15,53 +15,45 @@ import UIKit
 class ViewController: UIViewController {
     
     var imageArray = [#imageLiteral(resourceName: "AnimePicture2"), #imageLiteral(resourceName: "EP43D2jU8AEoKKt"), #imageLiteral(resourceName: "AnimePicture1"), #imageLiteral(resourceName: "EcKixJmU0AAorf8"), #imageLiteral(resourceName: "Image1"), #imageLiteral(resourceName: "EsVEqQiUcAAnbJi"), #imageLiteral(resourceName: "AnimePicture2-1"), #imageLiteral(resourceName: "Ei_icTzVoAAJYuq")]
-    var imageDisplay = Int()
-    var imageDisplayMax = Int()
+    var rightGestureImage = Int()
+    var leftGestureImage = Int()
     @IBAction func CGesture(_ sender: Any) {
         
     }
     // The code will get the pictures from the image litral array and bring them to the front, so they can be played in order.
-    @IBAction func LeftGesture(_ gestureRecognizer : UISwipeGestureRecognizer) {
+    @IBAction func RightGesture(_ gestureRecognizer : UISwipeGestureRecognizer) {
         print("Right Gesture Working")
         
-        print(imageDisplay)
-        MainViewPicture.image = imageArray[imageDisplay]
-        imageDisplay = (imageDisplay + 1) % imageArray.count
-
-//        if imageDisplay < imageDisplayMax {
-//            print(imageDisplay)
-//            MainViewPicture.image = imageArray[imageDisplay]
-//            imageDisplay += 1
-//
-//        } else {
-//            // this gets executed when imageDisplay is equal to the last index in the imageArray
-//            print(imageDisplay)
-//            MainViewPicture.image = imageArray[imageDisplay]
-//            imageDisplay = 0
-//
-//        }
+        if rightGestureImage < leftGestureImage {
+            print(rightGestureImage)
+            MainViewPicture.image = imageArray[rightGestureImage]
+            rightGestureImage += 1
+            
+        } else {
+            // this gets executed when imageDisplay is equal to the last index in the imageArray
+            print(rightGestureImage)
+            MainViewPicture.image = imageArray[rightGestureImage]
+            rightGestureImage = 0
+            
+        }
     }
     
     // TODO: make the left gesture go back to the previues picture.
     // The code will get the pictures from the image litral array and bring them to the front, so they can be played in order.
-    @IBAction func RightGesture(_ gestureRecognizer : UISwipeGestureRecognizer) {
-        print("Left")
-        print(imageDisplay)
-        MainViewPicture.image = imageArray[imageDisplay]
-        imageDisplay = (imageDisplay + 1) % imageArray.count
+    @IBAction func LeftGesture(_ gestureRecognizer : UISwipeGestureRecognizer) {
+        print("Left Gesture Working")
         
        //MainViewPicture.image = imageArray[imageDisplay]
-//        if imageDisplay > 0 {
-//            imageDisplay -= 1
-//            MainViewPicture.image = imageArray[imageDisplay]
-//            print(imageDisplay)
-//        }
-//
-//        else if imageDisplay == 0 {
-//            print(imageDisplay)
-//            imageDisplay = imageDisplayMax
-//            MainViewPicture.image = imageArray[imageDisplay]
-//        }
+        if rightGestureImage > 0 {
+            rightGestureImage -= 1
+            MainViewPicture.image = imageArray[rightGestureImage]
+            print(rightGestureImage)
+        }
+        else if rightGestureImage == 0 {
+            print(rightGestureImage)
+            rightGestureImage = leftGestureImage
+            MainViewPicture.image = imageArray[rightGestureImage]
+        }
     }
     
     @IBOutlet weak var MainViewPicture: UIImageView!
@@ -73,23 +65,23 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
 //        MainViewPicture.image = imageArray[Int.random(in: 0...5)]
         // To prevent from going into a infintie number (setting a bound here).
-//        imageDisplay = 0
-//        imageDisplayMax = imageArray.count-1
+        rightGestureImage = 0
+        leftGestureImage = imageArray.count-1
        // MainViewPicture.image = imageArray[0]
 
-        getRandomImage()
+//        getRandomImage()
         
     }
     
     // The Function to get the imagees from the internet aka API
-    func getRandomImage() {
-        let urlString = "https://api.waifu.pics/sfw/waifu"
-        let url = URL(string: urlString)!
-        
-        guard let data = try? Data(contentsOf: url) else {
-            return
-        }
-    }
+//    func getRandomImage() {
+//        let urlString = "https://api.waifu.pics/sfw/waifu"
+//        let url = URL(string: urlString)!
+//
+//        guard let data = try? Data(contentsOf: url) else {
+//            return
+//        }
+//    }
     //TODO: find something else to add here instead of adding an info button!
     @IBAction func infoButton(_ sender: Any) {
         print("Info Button Being Pressed")
