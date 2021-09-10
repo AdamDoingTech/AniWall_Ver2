f336f2be942b022a687ebaa3ae4e3f20a1bf6f12
Adam <olmpy@hotmail.com>
2021-09-09 22:10:20 -0600

    Cleaned the code

--- AniWallPaperAppV2/ViewController.swift
+++ AniWallPaperAppV2/ViewController.swift
@@ -10,18 +10,25 @@
 //let image = imageArray[Int.random(in: 0...5)]
 //MainViewPicture.image = image
 
+// L. Snippit
+//print(rightGestureImage)
+//MainViewPicture.image = imageArray[rightGestureImage]
+//rightGestureImage = (rightGestureImage + 1) % imageArray.count
+
 import UIKit
 
 class ViewController: UIViewController {
     
-    var imageArray = [#imageLiteral(resourceName: "AnimePicture2"), #imageLiteral(resourceName: "EP43D2jU8AEoKKt"), #imageLiteral(resourceName: "AnimePicture1"), #imageLiteral(resourceName: "EcKixJmU0AAorf8"), #imageLiteral(resourceName: "Image1"), #imageLiteral(resourceName: "EsVEqQiUcAAnbJi"), #imageLiteral(resourceName: "AnimePicture2-1"), #imageLiteral(resourceName: "Ei_icTzVoAAJYuq")]
+    let imageArray = [#imageLiteral(resourceName: "AnimePicture2"), #imageLiteral(resourceName: "EP43D2jU8AEoKKt"), #imageLiteral(resourceName: "AnimePicture1"), #imageLiteral(resourceName: "EcKixJmU0AAorf8"), #imageLiteral(resourceName: "Image1"), #imageLiteral(resourceName: "EsVEqQiUcAAnbJi"), #imageLiteral(resourceName: "AnimePicture2-1"), #imageLiteral(resourceName: "Ei_icTzVoAAJYuq")]
     var rightGestureImage = Int()
     var leftGestureImage = Int()
     @IBAction func CGesture(_ sender: Any) {
         
     }
+    
     // The code will get the pictures from the image litral array and bring them to the front, so they can be played in order.
     @IBAction func RightGesture(_ gestureRecognizer : UISwipeGestureRecognizer) {
+        // To make sure that the gesture is working
         print("Right Gesture Working")
         
         if rightGestureImage < leftGestureImage {
@@ -30,7 +37,7 @@ class ViewController: UIViewController {
             rightGestureImage += 1
             
         } else {
-            // this gets executed when imageDisplay is equal to the last index in the imageArray
+            // This gets executed when rightGestureImage is equal to the last index in the imageArray
             print(rightGestureImage)
             MainViewPicture.image = imageArray[rightGestureImage]
             rightGestureImage = 0
@@ -38,12 +45,10 @@ class ViewController: UIViewController {
         }
     }
     
-    // TODO: make the left gesture go back to the previues picture.
-    // The code will get the pictures from the image litral array and bring them to the front, so they can be played in order.
     @IBAction func LeftGesture(_ gestureRecognizer : UISwipeGestureRecognizer) {
+        // To make sure that the gesture is working
         print("Left Gesture Working")
         
-       //MainViewPicture.image = imageArray[imageDisplay]
         if rightGestureImage > 0 {
             rightGestureImage -= 1
             MainViewPicture.image = imageArray[rightGestureImage]
@@ -58,30 +63,28 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var MainViewPicture: UIImageView!
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-//        MainViewPicture.image = imageArray[Int.random(in: 0...5)]
+        //        MainViewPicture.image = imageArray[Int.random(in: 0...5)]
         // To prevent from going into a infintie number (setting a bound here).
         rightGestureImage = 0
         leftGestureImage = imageArray.count-1
-       // MainViewPicture.image = imageArray[0]
-
-//        getRandomImage()
+        // MainViewPicture.image = imageArray[0]
+        //        getRandomImage()
         
     }
     
     // The Function to get the imagees from the internet aka API
-//    func getRandomImage() {
-//        let urlString = "https://api.waifu.pics/sfw/waifu"
-//        let url = URL(string: urlString)!
-//
-//        guard let data = try? Data(contentsOf: url) else {
-//            return
-//        }
-//    }
+    //    func getRandomImage() {
+    //        let urlString = "https://api.waifu.pics/sfw/waifu"
+    //        let url = URL(string: urlString)!
+    //
+    //        guard let data = try? Data(contentsOf: url) else {
+    //            return
+    //        }
+    //    }
+    
     //TODO: find something else to add here instead of adding an info button!
     @IBAction func infoButton(_ sender: Any) {
         print("Info Button Being Pressed")
@@ -96,7 +99,5 @@ class ViewController: UIViewController {
     @IBAction func downloadButton(_ sender: Any) {
         print("Download Button Being poressed!")
     }
-    
-    
 }
 
